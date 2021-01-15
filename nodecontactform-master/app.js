cc721d77364f582df07960ecb23729d83499f0d4
Vh Long <vhlong@Vhs-MacBook-Air.local>
2021-01-16 03:49:01 +0700

    {FirstCommit}

---
+++ nodecontactform-master/app.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const nodemailer = require('nodemailer');
+const { getMaxListeners } = require('process');
+
+const app = express();
+
+// View engine setup
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+// Static folder
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.render('contact',{layout: false});
+});
+
+app.post('/send', (req, res) => {
+  const output = `
+    
+    
+    
+    <p>${req.body.message}</p>
+  `;
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+        
+        user: req.body.yourEmail, // generated ethereal user
+        pass: req.body.yourPwd  // generated ethereal password
+    },
+    tls:{
+      rejectUnauthorized:false
+    }
+  });
+
+  // setup email data with unicode symbols
+  let mailOptions = {
+      from: '"Nodemailer " <>', // sender address
+      to: req.body.sendTo, // list of receivers
+      subject: req.body.subject, // Subject line
+      text: 'Hello world?', // plain text body
+      html: output // html body
+  };
+  console.log(req.body);
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+          return console.log(error);
+      }
+      console.log('Message sent: %s', info.messageId);   
+      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+      res.render('contact', {msg:'Email has been sent',layout: false});
+  });
+  });
+
+app.listen(3000, () => console.log('Server started...'));
